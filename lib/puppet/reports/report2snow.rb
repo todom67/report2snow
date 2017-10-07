d328e991cbad9689a5bd2dadf7cc1c6be8a5b020
Timothy Odom <tim.odom@thinkahead.com>
2017-10-06 21:53:06 -0500

    add some payload

--- lib/puppet/reports/report2snow.rb
+++ lib/puppet/reports/report2snow.rb
@@ -1,6 +1,8 @@
 require 'puppet'
 require 'yaml'
 require 'json'
+require 'rest-client'
+require 'base64'
 
 Puppet::Reports.register_report(:report2snow) do
   desc "Send corrective changes to ServiceNow"
@@ -8,9 +10,9 @@ Puppet::Reports.register_report(:report2snow) do
   raise(Puppet::ParseError, "Servicenow report config file #{@configfile} not readable") unless File.exist?(@configfile)
 
   @config = YAML.load_file(@configfile)
-  API_URL = @config['api_url']
-  USERNAME = @config['username']
-  PASSWORD = @config['password']
+  SN_URL = @config['api_url']
+  SN_USERNAME = @config['username']
+  SN_PASSWORD = @config['password']
 
 	def process
     # Open a file for debugging purposes
@@ -18,11 +20,27 @@ Puppet::Reports.register_report(:report2snow) do
     debugFile.write("--- starting process ---\n")
     # We only want to send a report if we have a corrective change
     self.status == "changed" && self.corrective_change == true ? real_status = "#{self.status} (corrective)" : real_status = "#{self.status}" 
-    msg = "Puppet run resulted in a status: #{real_status} in the #{self.environment} environment"
-    debugFile.write("MSG: #{msg}\n")
+    msg = "Puppet run resulted in a status of '#{real_status}'' in the '#{self.environment}' environment"
 
     if real_status == 'changed (corrective)' then
-      debugFile.write("We have a #{real_status} so we are going to call service now\n")  
+      debugFile.write("We have a #{real_status} so we are going to call service now\n")
+      request_body_map = {
+        :active => 'false',
+        :category => 'Puppet Corrective Change',
+        :description => "#{msg}",
+        :escalation => '0',
+        :impact => '1',
+        :incident_state => '3',
+        :priority => '2',
+        :severity => '1',
+        :short_description => "Puppet Corrective Change on #{self.host}",
+        :state => '7',
+        :sys_created_by => 'Puppet but not Kermit',
+        :urgency => '1',
+        :work_notes => "Node Reports: [code]<a class='web' target='_blank' href='https://#{PUPPETCONSOLE}/#/node_groups/inventory/node/#{self.host}/reports'>Reports</a>[/code]"
+      }
+      debugFile.write("Payload: #{request_body_map}")
+      debugFile.write("Done!!\n")  
     end
     
     
